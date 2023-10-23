4f553e64114c548588fda51be662ed68ea76d97e
CalRhoads <calrrhoads@gmail.com>
2023-10-22 18:54:36 -0600

    complwtw lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const mainNode = document.querySelector('main#main');
+if (mainNode) {
+  const parentNode = mainNode.parentNode;
+  parentNode.removeChild(mainNode);
+}
+
+const newHeader = document.createElement("h1");
+const yourName = "Cal";
+newHeader.textContent = `${yourName} is the champion`;
+newHeader.setAttribute("id", "victory");
